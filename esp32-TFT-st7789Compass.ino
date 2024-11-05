3b260b39717011c27441c381c13c9dc6d454383e
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2024-11-05 15:49:05 +0100

    Update esp32-TFT-st7789Compass.ino

--- esp32-TFT-st7789Compass.ino
+++ esp32-TFT-st7789Compass.ino
@@ -6,12 +6,31 @@
 // Inicializácia GPS a TFT
 TinyGPSPlus gps;
 
-// Nastavenie pinov pre ST7789
-#define TFT_CS     15   // Môže byť vynechané, ak displej nemá CS pin
-#define TFT_RST    4
-#define TFT_DC     2
+#if defined(ARDUINO_FEATHER_ESP32) // Feather Huzzah32
+  #define TFT_CS         15
+  #define TFT_RST        4
+  #define TFT_DC         2
+  #define TFT_MOSI       23
+  #define TFT_SCK        18
+  #define TFT_MISO       19
+
+#elif defined(ESP8266)
+  #define TFT_CS         4
+  #define TFT_RST        16                                            
+  #define TFT_DC         5
+
+#else
+  #define TFT_CS         15
+  #define TFT_RST        4
+  #define TFT_DC         2
+  #define TFT_MOSI       23
+  #define TFT_SCK        18
+  #define TFT_MISO       19
+#endif
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+bool hasGPSFix = false;  // Sleduje stav GPS signálu
 
 void setup() {
   // Inicializácia sériovej komunikácie a displeja
@@ -19,10 +38,19 @@ void setup() {
   Serial2.begin(9600, SERIAL_8N1, 16, 17);  // RX/TX piny pre GPS modul
   
   // Inicializácia TFT displeja
-  tft.init(240, 320);  // Uprav podľa rozlíšenia displeja (napr. 240x240)
-  tft.invertDisplay(0);
+  tft.init(240, 320);  // Nastavenie rozlíšenia displeja 240x320
   tft.setRotation(1);  // Nastavenie rotácie displeja
+  tft.invertDisplay(0); // Vypnutie invertovania displeja (zmeniť na 1, ak farby zostávajú invertné)
   tft.fillScreen(ST77XX_BLACK);
+}
+
+void displayNoSignal() {
+  // Funkcia na zobrazenie chybového hlásenia pri strate signálu
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(30, 150);  // Nastavenie stredu obrazovky pre text
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(2);
+  tft.print("Nie je GPS signal!");
 }
 
 void loop() {
@@ -31,16 +59,19 @@ void loop() {
     gps.encode(Serial2.read());
   }
 
-  // Kontrola platnosti GPS signálu
   if (!gps.location.isValid()) {
-    // Ak GPS nemá platné dáta, zobraz upozornenie
-    tft.fillScreen(ST77XX_BLACK);  // Vymaže displej
-    tft.setCursor(30, 110);        // Nastaví pozíciu textu
-    tft.setTextColor(ST77XX_RED);
-    tft.setTextSize(2);
-    tft.print("Nie je GPS signal!");
+    // Ak GPS nemá platné dáta
+    if (hasGPSFix) {  // Skontroluje, či došlo k strate signálu
+      displayNoSignal();
+      hasGPSFix = false;
+    }
   } else {
     // Keď sú GPS dáta platné a zariadenie sa pohybuje
+    if (!hasGPSFix) {  // Ak bol signál obnovený, vyčistí displej
+      tft.fillScreen(ST77XX_BLACK);
+      hasGPSFix = true;
+    }
+
     if (gps.location.isUpdated() && gps.speed.isValid() && gps.course.isValid()) {
       float heading = gps.course.deg();  // Azimut/smer pohybu v stupňoch
 
@@ -54,7 +85,7 @@ void loop() {
       tft.print("Kompas");
 
       // Výpočet pozície šípky na displeji
-      int x_center = 120, y_center = 120;  // Stred displeja pre rozlíšenie 240x240
+      int x_center = 120, y_center = 160;  // Stred displeja pre rozlíšenie 240x320
       float arrow_length = 50;
       float rad = heading * PI / 180;  // Prevod uhla na radiány
 
@@ -72,11 +103,11 @@ void loop() {
       // Zobrazenie svetových strán
       tft.setCursor(110, 30);
       tft.print("N");  // Sever
-      tft.setCursor(200, 120);
+      tft.setCursor(200, 160);
       tft.print("E");  // Východ
-      tft.setCursor(120, 200);
+      tft.setCursor(120, 290);
       tft.print("S");  // Juh
-      tft.setCursor(30, 120);
+      tft.setCursor(30, 160);
       tft.print("W");  // Západ
 
       delay(1000);  // Obnovovacia frekvencia (1 sekunda)
